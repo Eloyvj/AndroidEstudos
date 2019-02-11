40b715e97b0cf593d221a93f8a400dfedb2ec4f7
Eloyvj <eloyvj@gmail.com>
2019-02-11 11:26:21 -0200

    Ajuste no metodo de compatibilidade para pegar a cor

--- HelloCompact/app/src/main/java/com/example/eloy/hellocompact/MainActivity.java
+++ HelloCompact/app/src/main/java/com/example/eloy/hellocompact/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.eloy.hellocompact;
 
+import android.content.Context;
 import android.os.Build;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
