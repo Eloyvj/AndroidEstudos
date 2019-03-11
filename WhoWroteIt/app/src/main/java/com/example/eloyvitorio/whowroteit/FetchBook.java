ec5135f63da307fe0654b2e2c5cfda683be639fe
Eloyvj <eloyvj@gmail.com>
2019-03-11 16:39:32 -0300

    Progresso ate a task 2.4

---
+++ WhoWroteIt/app/src/main/java/com/example/eloyvitorio/whowroteit/FetchBook.java
@@ -0,0 +1,29 @@
+package com.example.eloyvitorio.whowroteit;
+
+import android.os.AsyncTask;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.lang.ref.WeakReference;
+
+public class FetchBook extends AsyncTask<String, Void, String> {
+
+    private WeakReference<TextView> mTitleText;
+    private WeakReference<TextView> mAuthorText;
+
+    FetchBook(TextView titleText, TextView authorText) {
+        this.mTitleText = new WeakReference<>(titleText);
+        this.mAuthorText = new WeakReference<>(authorText);
+    }
+
+    @Override
+    protected String doInBackground(String... strings) {
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+        super.onPostExecute(s);
+    }
+}
