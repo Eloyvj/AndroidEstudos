667f5f3365a3e898bb6067503af7403a045411bc
Eloyvj <eloyvj@gmail.com>
2019-03-25 14:04:35 -0300

    Criacao do projeto para o challenge 10.1 Part B

---
+++ RoomWordsWithUpdate/app/src/main/java/com/example/eloyvitorio/roomwordswithupdate/WordRepository.java
@@ -0,0 +1,88 @@
+package com.example.eloyvitorio.roomwordswithupdate;
+
+import android.app.Application;
+import android.arch.lifecycle.LiveData;
+import android.os.AsyncTask;
+
+import java.util.List;
+
+public class WordRepository {
+
+    private WordDao mWordDao;
+    private LiveData<List<Word>> mAllWords;
+
+    WordRepository(Application application) {
+        WordRoomDatabase db = WordRoomDatabase.getDatabase(application);
+        mWordDao = db.wordDao();
+        mAllWords = mWordDao.getAllWords();
+    }
+
+    LiveData<List<Word>> getAllWords() {
+
+        return mAllWords;
+    }
+
+    // Method to invoke the insertAsyncTask method
+    public void insert (Word word) {
+        new insertAsyncTask(mWordDao).execute(word);
+    }
+
+    // Method to invoke the deleteAllAsyncTask method
+    public void deleteAll () {
+        new deleteAllWordsAsyncTask(mWordDao).execute();
+    }
+
+    // Method to invoke the deleteWordAsyncTask method
+    public void deleteWord (Word word) {
+        new deleteWordAsyncTask(mWordDao).execute(word);
+    }
+
+    // Intern class for insert a word action
+    private static class insertAsyncTask extends AsyncTask<Word, Void, Void> {
+
+        private WordDao mAsyncTaskDao;
+
+        insertAsyncTask(WordDao dao) {
+
+            mAsyncTaskDao = dao;
+        }
+
+        @Override
+        protected Void doInBackground(final Word... params) {
+            mAsyncTaskDao.insert(params[0]);
+            return null;
+        }
+    }
+
+    // Intern class for delete all words
+    private static class deleteAllWordsAsyncTask extends AsyncTask<Void, Void, Void> {
+
+        private WordDao mAsyncTaskDao;
+
+        deleteAllWordsAsyncTask(WordDao dao) {
+            mAsyncTaskDao = dao;
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            mAsyncTaskDao.deleteAll();
+            return null;
+        }
+    }
+
+    // Intern class for delete a word
+    private static class deleteWordAsyncTask extends AsyncTask<Word, Void, Void> {
+
+        private WordDao mAsyncTaskDao;
+
+        deleteWordAsyncTask(WordDao dao) {
+            mAsyncTaskDao = dao;
+        }
+
+        @Override
+        protected Void doInBackground(final Word... params) {
+            mAsyncTaskDao.deleteWord(params[0]);
+            return null;
+        }
+    }
+}
